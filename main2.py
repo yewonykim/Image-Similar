3271b09a321b0fa9c7296bf40bf8475afc427578
yewonykim <yja06220@gmail.com>
2023-12-07 17:29:45 +0900

    modify

--- main2.py
+++ main2.py
@@ -3,11 +3,11 @@ from calculating import calculate_similarity
 
 
 # 이미지 처리를 위한 폴더 경로
-directory = "C:/Users/user/Documents/Deep_learning/pokemon/images/images"
+directory = "./pokemon/images/images"
 
 # 이미지 처리를 위한 함수 호출
 fe, features, img_paths = process_images(directory)
 
 # 이미지 유사도 계산 및 시각화를 위한 함수 호출
-query_image_path = "C:/Users/user/Documents/Deep_learning/pokemon/images/images/doduo.png"
+query_image_path = "./pokemon/images/images/doduo.png"
 calculate_similarity(fe, features, img_paths, query_image_path)
